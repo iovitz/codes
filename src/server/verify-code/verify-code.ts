7dcb5f5823fd2c549ec3af04aae19b041c232253
iovitz <iovitz1999@gmail.com>
2023-05-04 14:36:56 +0800

    feat: 添加密码盐方案

--- src/server/verify-code/verify-code.ts
+++ src/server/verify-code/verify-code.ts
@@ -1,12 +1,23 @@
-
+import {createHmac} from 'crypto'
 export function getCode (length: number) {
   return Math.random()
       .toString(
-          // 10个数字+26个字母
+      // 10个数字+26个字母
           36,
       )
       .substring(
-          // 从第三位开始，第一位容易重复
-          3, 3 + length,
+      // 从第三位开始，第一位容易重复
+          3,
+          3 + length,
       )
+}
+
+export function cryptoPassword (password: string, salt = 'salt') {
+  const hash = createHmac('sha512', salt) /** 使用sha512算法进行hash*/
+  hash.update(password)
+  const value = hash.digest('hex')
+  return {
+    salt: salt,
+    passwordHash: value,
+  }
 }
